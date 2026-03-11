b66d5f8a2d7fc7b5cefd0e539d9207e7788d53f5
Doxygen CI <ci-arduino@invalid>
2026-03-11 22:25:14 +0000

    Deploy docs to GitHub Pages from commit 7a70187e65
    Commit: 7a70187e65bbf7fd23cb9632df400935bb222bc7
    GitHub Actions run: 22977128361

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,6 +1,8 @@
 var searchData=
 [
+  ['getcrcenabled',['getCRCEnabled',['../class_adafruit___a_d_s7128.html#a4a33755344e1fea7404ce4337b3ae4a3',1,'Adafruit_ADS7128']]],
   ['getcrcerror',['getCRCError',['../class_adafruit___a_d_s7128.html#a07e3daeaa8d7e5fdecf462428aad4a73',1,'Adafruit_ADS7128']]],
+  ['getdwcenabled',['getDWCEnabled',['../class_adafruit___a_d_s7128.html#aeb1e5c9610471361e4f5d6943f870a03',1,'Adafruit_ADS7128']]],
   ['geteventflags',['getEventFlags',['../class_adafruit___a_d_s7128.html#a25640c92b5082d17663da8d51adf6827',1,'Adafruit_ADS7128']]],
   ['geteventhighflags',['getEventHighFlags',['../class_adafruit___a_d_s7128.html#a967fd211833a89156cb22b36d83c480b',1,'Adafruit_ADS7128']]],
   ['geteventlowflags',['getEventLowFlags',['../class_adafruit___a_d_s7128.html#aa5637a43448a2d153ab5b8cad0ad1132',1,'Adafruit_ADS7128']]],
@@ -9,5 +11,7 @@ var searchData=
   ['getoversampling',['getOversampling',['../class_adafruit___a_d_s7128.html#a635166a09885516873cf9f97fda01379',1,'Adafruit_ADS7128']]],
   ['getrecent',['getRecent',['../class_adafruit___a_d_s7128.html#ad54e40d87e3b23a4bac9932b998a0772',1,'Adafruit_ADS7128']]],
   ['getrms',['getRMS',['../class_adafruit___a_d_s7128.html#a991cb9c45ce041a583895d041b6e335d',1,'Adafruit_ADS7128']]],
-  ['getstatisticsenabled',['getStatisticsEnabled',['../class_adafruit___a_d_s7128.html#a65f920b6ce8350af7f0763e48fb028e6',1,'Adafruit_ADS7128']]]
+  ['getrmsenabled',['getRMSEnabled',['../class_adafruit___a_d_s7128.html#a7261e40abd74a47ce1fa09b4557a0cb4',1,'Adafruit_ADS7128']]],
+  ['getstatisticsenabled',['getStatisticsEnabled',['../class_adafruit___a_d_s7128.html#a65f920b6ce8350af7f0763e48fb028e6',1,'Adafruit_ADS7128']]],
+  ['getzcdoutputenabled',['getZCDOutputEnabled',['../class_adafruit___a_d_s7128.html#a785cac5c0e4a1311f0ed0336b664be81',1,'Adafruit_ADS7128']]]
 ];
